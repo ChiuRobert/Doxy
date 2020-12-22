1da7cfe28f6d5ec77ae98b4ecdfe2c6be0905a86
Robert Chiu <chiu.robert96@gmail.com>
2020-12-22 11:54:02 +0200

    Added dummy test

---
+++ Assets/Editor/Tests/PipeTest.cs
@@ -0,0 +1,22 @@
+﻿using Editor.Tests.TestCase;
+using NUnit.Framework;
+
+namespace Editor.Tests
+{
+    public class PipeTest : DoxyTestCase
+    {
+        protected override void OpenScene()
+        {
+        }
+
+        protected override void SetUpTestSpecific()
+        {
+        }
+        
+        [Test]
+        public void PipeDummy_Test()
+        {
+            Assertions.AreEqual("ceapa", "ceapa");
+        }
+    }
+}
